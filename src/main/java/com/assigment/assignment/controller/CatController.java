b62fbf476bf8591326ae4ba74ddb268c74e24961
Melendez <henryomar.melendez@fiserv.com>
2023-10-30 20:39:14 -0400

    maria db working

--- src/main/java/com/assigment/assignment/controller/CatController.java
+++ src/main/java/com/assigment/assignment/controller/CatController.java
@@ -3,6 +3,7 @@ package com.assigment.assignment.controller;
 import com.assigment.assignment.model.Cat;
 import com.assigment.assignment.repositories.CatRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("api/v1")
 public class CatController {
+    @Autowired
     private CatRepository catRepository;
 
     @GetMapping("/test")
